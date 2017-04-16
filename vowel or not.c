45384553995851d48ea2104e72c1d3a375be495a
Harithanair <chinjuharitha23@gmail.com>
2017-04-16 20:23:34 +0530

    Update vowel or not.c

--- vowel or not.c
+++ vowel or not.c
@@ -4,7 +4,7 @@ int main()
 char ch;
 printf("enter the character\n');
 scanf("%c",&ch);
-if(ch=='a'||ch==A\ch=='e'\\ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+if(ch=='a'||ch==A||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
 printf("%cis a vowel\n",ch);
 else
 printf("%cis not a vowel"\n",ch);
