101f0503b47b895f150fac1cfd2b85ca6c1ce76a
Haritha.H <chinjuharitha23@gmail.com>
2017-06-27 09:17:55 -0700

    Create encode.c

---
+++ encode.c
@@ -0,0 +1,17 @@
+#include<stdio.h.
+#include<string.h.
+int main()
+{
+char[30],temp[30];
+int spcnt[30],x,j,m;
+printf("enter the string: ");
+gets(string);
+for(x=0;string[x];x++)
+{
+string[x]=string[x]+3;
+}
+printf("\n");
+printf("enter the encoded string %s",string);
+string[x]=string[x]-3;
+return 0;
+}
